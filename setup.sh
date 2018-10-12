dbf2cdbc540a1df7a55a0bf031cab1aa22943bea
Robin Klussmann <32791078+siw36@users.noreply.github.com>
2018-10-12 15:16:04 +0200

    first test code for rhel based os

--- setup.sh
+++ setup.sh
@@ -19,15 +19,27 @@ case $HOSTOS in
 	sudo yum -y -q update
 	printf ">>>Update finished\n\n"
 	# Install packages
-	printf "***Install packages: git \n"
-	sudo yum -y -q install python2 python-simplejson
+	printf "***Install packages: git openssh openssh-clients scp vim ansible\n"
+	sudo yum -y -q git openssh openssh-clients scp vim ansible wget
+	printf ">>>Installation finished\n\n"
+	printf "***Install azure-cli: adding azure repo key and repo\n"
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+	sudo yum -y -q install azure-cli
+	printf ">>>Installation finished\n\n"
+	printf "***Install openshift-cli: downloading oc binary from github, placing it in /usr/bin\n"
+	printf "***This script installs version v3.11.0 of oc\n"
+	printf "***Check: https://github.com/openshift/origin/releases for a newer version\n"
+	sudo wget -O oc-client.tar.gz -P /tmp/ https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+	sudo tar -zxvf /tmp/oc-client.tar.gz
+	sudo mv /tmp/oc /usr/bin/oc
 	printf ">>>Installation finished\n\n"
 	exit 0
 	;;
 	######################################################################################
 	# HOST OS IS UBUNTU
 	######################################################################################
-	'"Ubuntu"')
+	'"Ubuntu"'|'"Debian GNU/Linux"')
 	# Update
 	printf "***Update the System\n"
 	apt-get -y -q update && sudo apt-get -y -q upgrade
@@ -36,21 +48,6 @@ case $HOSTOS in
 	printf "***Install packages: python python-simplejson\n"
 	apt-get -y -q install python2 python-simplejson
 	printf ">>>Installation finished\n\n"
-	exit 0
-	;;
-	######################################################################################
-	# HOST OS IS DEBIAN
-	######################################################################################
-	'"Debian GNU/Linux"')
-	# Update
-	printf "***Update the System\n"
-	apt -y -q update && apt -y -q upgrade
-	printf ">>>Update finished\n\n"
-	# Install Python modules
-	printf "***Install packages: python python-simplejson\n"
-	apt -y -q install python
-	printf ">>>Installation finished\n\n"
-
 	exit 0
 	;;
 	*)
