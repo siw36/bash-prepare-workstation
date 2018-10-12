1e7baf5bea0b1da6ed721f8f995f3e38d96dfad7
Robin Klussmann <32791078+siw36@users.noreply.github.com>
2018-10-12 18:40:53 +0200

    initial release (testing)

--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 # Detect the host os type
 HOSTOS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
-# Update System
-# Install packages
-# Set Aliases
-
-
+######################################################################################
+# OS SPECIFIC
+######################################################################################
 
 case $HOSTOS in
 	######################################################################################
@@ -19,20 +17,13 @@ case $HOSTOS in
 	sudo yum -y -q update
 	printf ">>>Update finished\n\n"
 	# Install packages
-	printf "***Install packages: git openssh openssh-clients scp vim ansible\n"
-	sudo yum -y -q git openssh openssh-clients scp vim ansible wget
+	printf "***Install packages: git openssh openssh-clients scp vim ansible wget curl\n"
+	sudo yum -y -q git openssh openssh-clients scp vim ansible wget curl
 	printf ">>>Installation finished\n\n"
 	printf "***Install azure-cli: adding azure repo key and repo\n"
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 	sudo yum -y -q install azure-cli
-	printf ">>>Installation finished\n\n"
-	printf "***Install openshift-cli: downloading oc binary from github, placing it in /usr/bin\n"
-	printf "***This script installs version v3.11.0 of oc\n"
-	printf "***Check: https://github.com/openshift/origin/releases for a newer version\n"
-	sudo wget -O oc-client.tar.gz -P /tmp/ https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-	sudo tar -zxvf /tmp/oc-client.tar.gz
-	sudo mv /tmp/oc /usr/bin/oc
 	printf ">>>Installation finished\n\n"
 	exit 0
 	;;
@@ -42,11 +33,18 @@ case $HOSTOS in
 	'"Ubuntu"'|'"Debian GNU/Linux"')
 	# Update
 	printf "***Update the System\n"
-	apt-get -y -q update && sudo apt-get -y -q upgrade
+	sudo apt-get -y -q update && sudo apt-get -y -q upgrade
 	printf ">>>Update finished\n\n"
-	# Install Python modules
-	printf "***Install packages: python python-simplejson\n"
-	apt-get -y -q install python2 python-simplejson
+	# Install packages
+	printf "***Install packages: git openssh openssh-clients scp vim ansible wget curl\n"
+	sudo apt-get -y -q git openssh openssh-clients scp vim ansible wget curl
+	printf ">>>Installation finished\n\n"
+	printf "***Install azure-cli: adding azure repo key and repo\n"
+	AZ_REPO=$(lsb_release -cs)
+	echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+	curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get -y -q install apt-transport-https azure-cli
 	printf ">>>Installation finished\n\n"
 	exit 0
 	;;
@@ -54,3 +52,34 @@ case $HOSTOS in
 	echo "Unknown OS"
 	exit 1
 esac
+
+
+######################################################################################
+# UNIVERSAL
+######################################################################################
+### SET ALIASES
+echo "alias vi='vim'" >> ~/.bashrc
+
+### VIM CONFIG
+echo 'set number' >> ~/.vimrc
+
+### INSTALL OPENSHIFT CLIENT
+printf "***Install openshift-cli: downloading oc binary from github, placing it in /usr/bin\n"
+printf "***This script installs version v3.11.0 of oc\n"
+printf "***Check: https://github.com/openshift/origin/releases for a newer version\n"
+sudo wget -O oc-client.tar.gz -P /tmp/ https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+sudo tar -zxvf /tmp/oc-client.tar.gz
+sudo mv /tmp/oc /usr/bin/oc
+printf ">>>Installation finished\n\n"
+
+### INSTALL AWS CLIENT
+printf "***Install awscli: installing pip followed by awscli\n"
+sudo curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py --user
+echo 'export PATH=~/.local/bin:$PATH' >> ~/.bash_profile
+sudo source ~/.bash_profile
+pip install awscli --upgrade --user
+printf ">>>Installation finished\n\n"
+
+### REFRESH CURRENT BASH SESSION
+sudo source ~/.bash_profile
